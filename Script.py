a70fa36dbe75b9dd1a5f919c0a1eb759e958ad87
Đức Dương <d3nhatv0lam@gmail.com>
2024-03-07 19:44:59 +0700

    commit

---
+++ Script.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+from tkinter import filedialog
+import keyboard
+import pyautogui
+import unidecode
+import sys , time
+
+root = tk.Tk()
+root.withdraw()
+
+print("Load data: ")
+file_path = filedialog.askopenfilename()
+
+try:
+    with open(file_path,'r',encoding='utf-8') as f:
+        data = f.read()
+        data = unidecode.unidecode(data)
+except:
+    print("Pls Choose Code File or Txt File")
+    sys.exit(0)
+
+
+Line = data.split("\n")
+print(data)
+
+print("Press F2 to Copy this!") 
+
+keyboard.wait("F2")
+pyautogui.PAUSE = 0.01
+
+
+
+def isStop():
+    if keyboard.is_pressed('Esc'):
+        sys.exit(0)
+
+def Default_Paste():
+
+    for line in Line:
+        for char in line:
+            isStop()
+            pyautogui.press(char)
+        pyautogui.press('Enter')
+
+def Python_Paste():
+
+    CurLspace = len(Line[0]) - len(Line[0].lstrip())
+    defaultspace = 4
+
+    for line in Line:
+        backspaceCnt = len(line) - len(line.lstrip())
+
+        line = line.lstrip()
+
+        if (line != ''):
+            isSpace = CurLspace - backspaceCnt
+            if (isSpace > 0):
+                for i in range(0,isSpace//defaultspace):
+                    pyautogui.keyDown('shift')
+                    pyautogui.press('tab')
+                    time.sleep(0.1)
+                    pyautogui.keyUp('shift')
+            CurLspace = backspaceCnt
+
+
+        if line != '':
+            line += ' '
+
+        for char in line:
+            isStop()
+            pyautogui.press(char)
+
+        pyautogui.press("Enter")
+
+
+
+def C_Cpp_Paste():
+
+    for line in Line:
+        line = line.lstrip()
+        if line != '':
+            line += ' '
+        for char in line:
+            isStop()
+            pyautogui.press(char)
+        pyautogui.press('Enter')
+
+
+
+def Paste(extension):
+
+    match extension:
+        case "py":
+            Python_Paste()
+        case "c":
+            C_Cpp_Paste()
+        case "cpp":
+            C_Cpp_Paste()
+        case _:
+            Default_Paste()
+
+
+Paste(file_path.split(".")[-1])
+
+
